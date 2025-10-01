5c3d132caf69f661b2c9e9b263a66ace3aed41c2
Tohutson <tohutson@example.com>
2025-09-30 20:36:17 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/treyhutson/gym_occupancy/GymOccupancyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.treyhutson.gym_occupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymOccupancyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymOccupancyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treyhutson/gym_occupancy/GymOccupancyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treyhutson.gym_occupancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymOccupancyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
