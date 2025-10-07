49dfc397f836db7e37bdbe2f3724ac85b6662e86
Tohutson <tohutson@example.com>
2025-10-06 21:12:50 -0400

    Add query for findLatest

--- src/main/java/com/treyhutson/gym_occupancy/repository/FacilityCountRepository.java
+++ src/main/java/com/treyhutson/gym_occupancy/repository/FacilityCountRepository.java
@@ -2,8 +2,20 @@ package com.treyhutson.gym_occupancy.repository;
 
 import com.treyhutson.gym_occupancy.model.FacilityCount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FacilityCountRepository extends JpaRepository<FacilityCount, Long> {
+    @Query("""
+        SELECT fc FROM FacilityCount fc
+        WHERE fc.recordedAt IN (
+            SELECT MAX(fc2.recordedAt)
+            FROM FacilityCount fc2
+            GROUP BY fc2.locationName
+        )
+        """)
+    List<FacilityCount> findLatestPerFacility();
 }
